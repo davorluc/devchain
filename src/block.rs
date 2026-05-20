40208e43642dd5b2ceca4b917ebffeac9ffe473e
davorluc <d.lucic4@hotmail.com>
2026-05-20 23:45:50 +0200

    added proper and nonce generation

--- src/block.rs
+++ src/block.rs
@@ -1,5 +1,6 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
+#[allow(dead_code)]
 pub struct Block {
     index: i32,
     timestamp: u64,
@@ -10,9 +11,12 @@ pub struct Block {
 }
 
 impl Block {
-    pub fn new(index: i32, data: String, previous_hash: String, nonce: i32) -> Self {
+    pub fn new(index: i32, data: String, previous_hash: String /*nonce: i32*/) -> Self {
         let timestamp = Self::current_timestamp();
-        let hash = Self::calculate_hash(index, timestamp, &data, &previous_hash, nonce);
+        let nonce: i32 = 0;
+        let (hash, nonce) = Self::calculate_hash(index, timestamp, &data, &previous_hash, nonce);
+        println!("{}", nonce);
+        println!("{}", hash);
 
         Self {
             index,
@@ -36,17 +40,26 @@ impl Block {
         timestamp: u64,
         data: &str,
         previous_hash: &str,
-        nonce: i32,
-    ) -> String {
+        mut nonce: i32,
+    ) -> (String, i32) {
         use hex;
         use sha2::{Digest, Sha256};
 
-        let input = format!("{}{}{}{}{}", index, timestamp, data, previous_hash, nonce);
+        // let input = format!("{}{}{}{}{}", index, timestamp, data, previous_hash, nonce);
 
-        let mut hasher = Sha256::new();
-        hasher.update(input);
-        let hash = hasher.finalize();
-        let hash_string = hex::encode(hash);
-        hash_string
+        let hash_string: String = loop {
+            let input = format!("{}{}{}{}{}", index, timestamp, data, previous_hash, nonce);
+            let mut hasher = Sha256::new();
+            hasher.update(input);
+            let hash = hasher.finalize();
+            let current_hash = hex::encode(hash);
+
+            if current_hash.starts_with("00") {
+                break current_hash;
+            }
+            nonce += 1;
+        };
+
+        (hash_string, nonce)
     }
 }
