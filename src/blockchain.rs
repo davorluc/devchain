a8825b9df18bfaa273e5d2f23365717ce3604ed3
davorluc <d.lucic4@hotmail.com>
2026-05-29 18:28:35 +0200

    add block function and genesis block overhaul

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,23 +1,37 @@
 use crate::block::Block;
+use crate::transaction::Transaction;
 
 #[allow(dead_code)]
 pub struct Blockchain {
     pub chain: Vec<Block>,
+    pub mempool: Vec<Transaction>,
 }
 
 impl Blockchain {
     pub fn new() -> Self {
+        let genesis_transactions = vec![Transaction::new(
+            "Alice".to_string(),
+            "Bob".to_string(),
+            100,
+        )];
         let genesis_block = Block::new(
             0,
-            "I will do what I must.".to_string(),
+            genesis_transactions,
+            // "I will do what I must.".to_string(),
             "0000000000000000000000000000000000000000000000000000000000000000".to_string(),
         );
 
-        let mut chain: Vec<Block> = Vec::new();
-        chain.push(genesis_block);
-
-        Self { chain }
+        Self {
+            chain: vec![genesis_block],
+            mempool: Vec::new(),
+        }
     }
 
-    fn _add_block() {}
+    fn _add_block(&mut self, mempool: Vec<Transaction>) -> () {
+        let prev_block = self.chain.last().unwrap();
+
+        let new_block: Block = Block::new(prev_block.index + 1, mempool, prev_block.hash.clone());
+        self.chain.push(new_block);
+        self.mempool.clear();
+    }
 }
