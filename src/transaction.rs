88827a9031058e094c8485484fea85bb4f4f088b
davorluc <d.lucic4@hotmail.com>
2026-05-29 18:20:26 +0200

    implemented initial transaction model

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,5 +1,18 @@
-struct _Transaction {
+use serde::Serialize;
+
+#[derive(Debug, Clone, Serialize)]
+pub struct Transaction {
     sender: String,
     receiver: String,
-    amoung: i32,
+    amount: i64,
+}
+
+impl Transaction {
+    pub fn new(sender: String, receiver: String, amount: i64) -> Self {
+        Self {
+            sender,
+            receiver,
+            amount,
+        }
+    }
 }
