d44ba7e81093fff1a1f891a70a15e8bb4c435fc6
davorluc <d.lucic4@hotmail.com>
2026-05-29 18:44:17 +0200

    added dummy transactions

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,16 @@ mod blockchain;
 mod transaction;
 
 use crate::blockchain::Blockchain;
+use crate::transaction::Transaction;
 
 fn main() {
-    let _blockchain = Blockchain::new();
+    let mut blockchain = Blockchain::new();
+
+    let transaction1 = Transaction::new("Moff Gideon".to_string(), "Din Djarin".to_string(), 1000);
+    let transaction2 = Transaction::new("Din Djarin".to_string(), "Din Grogu".to_string(), 10);
+    let transaction3 = Transaction::new("Din Grogu".to_string(), "Hot Dog Man".to_string(), 10);
+
+    blockchain.mempool.push(transaction1);
+    blockchain.mempool.push(transaction2);
+    blockchain.mempool.push(transaction3);
 }
